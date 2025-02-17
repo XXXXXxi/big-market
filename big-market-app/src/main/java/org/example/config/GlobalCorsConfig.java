711511f2979465d358f3673d7af2c399098264e9
xiewenbin <121350551@qq.com>
2025-02-17 21:46:51 +0800

    feat: ：跨域bug

---
+++ big-market-app/src/main/java/org/example/config/GlobalCorsConfig.java
@@ -0,0 +1,29 @@
+package org.example.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Classname GlobalCorsConfig
+ * @Description 跨域全局配置
+ * @Date 2025/2/17 21:41
+ * @Created by 12135
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(configSource);
+    }
+}
